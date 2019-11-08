18b883a55b2e8564c79add7a26623cbb9075a895
Sienna Rose Sacramento <sienna.friends@yahoo.com>
2019-11-08 15:01:02 -0800

    Succeeded in using Vector2 and TRansform, but must fix position

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,14 +7,20 @@ public class MapGenerator : MonoBehaviour
     public Transform tilePrefab;
     public Vector2 mapSize;
 
+    public void Start()
+    {
+        GenerateMap();
+    }
+
     public void GenerateMap()
     {
         for(int x = 0; x < mapSize.x; x++)
         {
             for(int y = 0; y < mapSize.y; y++)
             {
-                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x,-mapSize.y + 0.5f + y);
+                Vector2 tilePosition = new Vector2(-mapSize.x / 2 + 0.5f + x,-mapSize.y + 0.5f + y);
                 Transform newTile = Instantiate(tilePrefab);
+                newTile.position = tilePosition;
             }
         }
     }
