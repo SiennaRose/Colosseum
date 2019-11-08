463b456dc729355cb6d9c7a5e1696486e08f273b
Sienna Rose Sacramento <sienna.friends@yahoo.com>
2019-11-08 14:35:35 -0800

    Switching from tilemap to prefab for tile organization

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Transform tilePrefab;
+    public Vector2 mapSize;
+
+    public void GenerateMap()
+    {
+
+    }
+}
