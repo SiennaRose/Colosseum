5a23dc9c59a4ded583bb15123036375555c60140
Sienna Rose Sacramento <sienna.friends@yahoo.com>
2019-11-08 15:39:17 -0800

    Added top wall tiles in the MapGenerator

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     public Transform tilePrefab;
+    public Transform topWallPrefab; 
     public Vector2 mapSize;
 
     public void Start()
     {
+        mapSize.x = 17;
+        mapSize.y = 7;
         GenerateMap();
     }
 
@@ -18,9 +21,17 @@ public class MapGenerator : MonoBehaviour
         {
             for(int y = 0; y < mapSize.y; y++)
             {
-                Vector2 tilePosition = new Vector2(-mapSize.x / 2 + 0.5f + x,-mapSize.y + 0.5f + y);
+                Vector2 tilePosition = new Vector2(-mapSize.x + 0.5f + x,-mapSize.y/2 + 0.5f + y);
                 Transform newTile = Instantiate(tilePrefab);
                 newTile.position = tilePosition;
+
+                //adding top wall tiles
+                if(y == (mapSize.y-1))
+                {
+                    Vector2 wallPosition = new Vector2(-mapSize.x/2 + 0.5f + x, -mapSize.y / 2 + 0.5f + y+1);
+                    Transform newWallTile = Instantiate(topWallPrefab);
+                    newWallTile.position = wallPosition;
+                }
             }
         }
     }
