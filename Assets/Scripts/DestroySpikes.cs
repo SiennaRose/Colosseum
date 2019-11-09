f541cc587ddc0d59ae58d2455336a63bfb1f1db3
Sienna Rose Sacramento <sienna.friends@yahoo.com>
2019-11-08 22:46:59 -0800

    made destroy spikes script

---
+++ Assets/Scripts/DestroySpikes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySpikes : MonoBehaviour
+{
+    public float lifetime = 10f;
+
+    void Update()
+    {
+        if(lifetime > 0)
+        {
+            lifetime -= Time.deltaTime;
+            if (lifetime <= 0)
+                Destruction();
+        }    
+    }
+    public void Destruction()
+    {
+        Destroy(this.gameObject);
+    }
+}
