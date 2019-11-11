85173f7bc13eb447c1b0987b6cf0b507b60aaedf
Sienna Rose Sacramento <sienna.friends@yahoo.com>
2019-11-10 16:48:52 -0800

    added menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject Panel_Menu;
+    public GameObject PlayButton;
+    public GameObject QuitButton;
+
+    public void Awake()
+    {
+        Panel_Menu = GameObject.Find("Panel_Menu");
+        Panel_Menu.SetActive(true);
+
+        PlayButton = GameObject.Find("PlayButton");
+        QuitButton = GameObject.Find("QuitButton");
+    }
+
+    public void playButtonClicked()
+    {
+        UIController.Instance.PlayGame();
+    }
+
+    public void quitButtonClicked()
+    {
+        UIController.Instance.QuitGame();
+    }
+}
